f5a7a188175235452d607b65f09b82204ba59dbe
carlvin <calvinokumu@gmail.com>
2023-07-19 18:02:12 +0300

    small changes

--- simple-crm-app/config/settings/dev.py
+++ simple-crm-app/config/settings/dev.py
@@ -6,10 +6,10 @@ SECRET_KEY="django-insecure-h*wprd2xbt#!jq#rrkr_ww2h(md#=bm&a0ok4h!tp(081%+6)6"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME','carlhubdb'),
-        'USER': os.getenv('DB_USER','carlhub'),
-        'PASSWORD': os.getenv('DB_PASS','1234'),
-        'HOST': os.getenv('DB_HOST','localhost'),
-        'PORT':os.getenv('DB_PORT','5432'),
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
+        'HOST': os.getenv('DB_HOST'),
+        'PORT':"",
     }
 }
