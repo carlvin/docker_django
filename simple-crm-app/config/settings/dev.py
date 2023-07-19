b31e5b46426e072fccfebcd3e375081627986815
carlvin <calvinokumu@gmail.com>
2023-07-19 18:53:37 +0300

    small changes

--- simple-crm-app/config/settings/dev.py
+++ simple-crm-app/config/settings/dev.py
@@ -1,6 +1,12 @@
 from .base import *
 DEBUG = True
 
+CSRF_TRUSTED_ORIGINS = ['https://*.carlhub.com','https://*.127.0.0.1']
+
+INTERNAL_IPS = ['127.0.0.1',]
+
+ALLOWED_HOSTS = ['carlhub.com','localhost','127.0.0.1']
+
 SECRET_KEY="django-insecure-h*wprd2xbt#!jq#rrkr_ww2h(md#=bm&a0ok4h!tp(081%+6)6"
 
 DATABASES = {
