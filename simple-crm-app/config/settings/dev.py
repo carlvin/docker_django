461551110efc49776b8ce4860fe11bd70c5bec9e
carlvin <calvinokumu@gmail.com>
2023-07-20 10:09:55 +0300

    small changes

--- simple-crm-app/config/settings/dev.py
+++ simple-crm-app/config/settings/dev.py
@@ -9,6 +9,9 @@ ALLOWED_HOSTS = ['carlhub.com','localhost','127.0.0.1']
 
 SECRET_KEY="django-insecure-h*wprd2xbt#!jq#rrkr_ww2h(md#=bm&a0ok4h!tp(081%+6)6"
 
+MEDIA_ROOT = "/home/media"
+STATIC_ROOT = "/home/static"
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
